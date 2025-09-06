1b6cdd0cdb95d80070ec612f393b40fe08b6dbd5
Prakhar Sharan <prakharsharan27@gmail.com>
2025-09-06 09:22:33 +0530

    Initial Commit

---
+++ src/main/java/com/jenkins/App.java
@@ -0,0 +1,14 @@
+package com.jenkins;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println( "Started Jenkin Project" );
+    }
+}
